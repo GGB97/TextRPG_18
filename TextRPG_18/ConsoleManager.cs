5e864bd04fa64648af1e6eba39c5b9c071fa9d2c
Shin004 <tktmdmswl01@gmail.com>
2024-01-09 17:56:52 +0900

    ConsoleManager 생성
    콘솔 색상 변경할시 사용할 함수

---
+++ TextRPG_18/ConsoleManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class ConsoleManager
+{
+    static public void RedColor(string str)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(str);
+        Console.ResetColor();
+
+    }
+
+
+}
