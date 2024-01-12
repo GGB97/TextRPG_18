91b7c14d0e25c74ebac02027ecba110bd4ad8c83
Shin004 <tktmdmswl01@gmail.com>
2024-01-12 23:13:46 +0900

    직업 생성및 초기화

--- TextRPG_18/Job.cs
+++ TextRPG_18/Job.cs
@@ -17,16 +17,12 @@ namespace TextRPG_18
         public int def;
         public float criticalChance;
         public float criticalDamage;
-
+        public int turn =0;
+        public bool turnfalse = false;
 
         public string Skill_name1;
         public string Skill_name2;
 
-
-        int PMhp;
-        int PMmp;
-        int PMatk;
-        int PMdef;
 
         //회피율 (공통) , 기본 공격력 업 (전사) 치명타 확률(기사), 치명타 피해량(마법사) 
 
@@ -43,7 +39,10 @@ namespace TextRPG_18
 
         public void Pick(Player player) // 직업선택시 스택 적용
         {
-            player.job = this.name;
+            playermax.maxHp = hp;
+            playermax.maxmp = mp;
+            playermax.atk = atk;
+            playermax.dfs = def;
             player.hp = this.hp;
             player.mp = this.mp;
             player.atk = this.atk;
@@ -59,7 +58,7 @@ namespace TextRPG_18
         }
 
 
-        public virtual void skill_1(Monster[] mon, Player player) //범위계
+        public virtual void skill_1(List<Monster> mon, Player player) //범위계
         {
             
         }
@@ -77,12 +76,19 @@ namespace TextRPG_18
         {
             return "DefaultName1";
         }
+
+        public virtual bool Initialization(Player player)//초기화
+        {
+            return false;
+            
+        }
+
     }
 
     public class Warrior : Job
     {
         public string Skill_name1 = "광 : 마나 10을 사용해 모든 몬스터에게 참격을 가합니다";
-        public string Skill_name2 = "굶주림 :자신의 피를 20% 깎고 기본 공격력을 20%증가합니다 (3턴동안)";
+        public string Skill_name2 = "굶주림 :자신의 전체 피에서 20% 깎고 기본 공격력을 30%증가시킵니다 (3턴동안)";
 
         public Warrior(string name, int hp, int mp, int atk, int def, float criticalChance, float criticalDamage) : base(name, hp, mp, atk, def, criticalChance, criticalDamage)
         {
@@ -95,14 +101,44 @@ namespace TextRPG_18
             return 1;
         }
 
-        public override void skill_1(Monster[] mon, Player player)
+        public override void skill_1(List<Monster> mon, Player player)
         {
-            //player.
+            player.mp -= 10;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n마나 10이 소비되었습니다.");
+            Console.WriteLine(player.name + " (이)가 거대한 참격을 준비합니다!");
+            Console.ResetColor();
+            Thread.Sleep(500);
+            Console.WriteLine($"=====================================================");
+            foreach (var item in mon) 
+            {
+                item.hp -= player.PlayerDamage();
+
+                Console.WriteLine($"\n{player.name} (이)가 {item.name}을(를) 공격!");
+                Console.WriteLine($"{item.name}은(는) -{player.atk}의 데미지를 입었다!\n");
+                Thread.Sleep(600);
+
+                if (item.hp <= 0)
+                {
+                    item.hp = 0;
+                    item.live = "dead";
+                }
+
+
+            }
+            Console.WriteLine($"=====================================================");
         }
 
         public override void Skill_2(Player player)
         {
-         
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n 전체 HP에서 20%이 감소되었습니다.");
+            Console.WriteLine($" {player.name} (이)가 피에 굶주림니다. 공격력이 30%증가합니다");
+            Console.ResetColor();
+
+            turnfalse = true;
+            player.hp -= (int)(playermax.maxHp * 0.2); //20% 빠짐
+            player.atk += (int)(atk * 0.3); //30% 증가
         }
 
         public override string GetName1()
@@ -114,9 +150,20 @@ namespace TextRPG_18
             return Skill_name2;
         }
 
-        public void Initialization()//초기화
+        public override bool Initialization(Player player)//초기화
         {
-
+            if (turnfalse)
+            {
+                if (turn >= 3)
+                {
+                    turnfalse = false;
+                    turn = 0;
+                    player.atk = playermax.atk; //다시 돌려놓기
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
         }
 
 
@@ -133,7 +180,7 @@ namespace TextRPG_18
         {
             return 2;
         }
-        public override void skill_1(Monster[] mon, Player player)
+        public override void skill_1(List<Monster> mon, Player player)
         {
 
         }
@@ -155,7 +202,7 @@ namespace TextRPG_18
     public class Mage : Job 
     {
         public string Skill_name1 = "화 : 마나 15를 이용해 적 모두에게 파이어 볼을";
-        public string Skill_name2 = "";
+        public string Skill_name2 = "욕망: 마나 15를 이용해 자신의 치명타 피해를 30% 증가합니다";
         public Mage(string name, int hp, int mp, int atk, int def, float criticalChance, float criticalDamage) : base(name, hp, mp, atk, def, criticalChance, criticalDamage)
         {
         }
@@ -165,11 +212,11 @@ namespace TextRPG_18
             return 3;
         }
 
-        public override void skill_1(Monster[] mon, Player player)
+        public override void skill_1(List<Monster> mon, Player player)
         {
             foreach (var item in mon) 
             {
-                mon.
+                //
             }
         }
 
--- TextRPG_18/JobManager.cs
+++ TextRPG_18/JobManager.cs
@@ -31,6 +31,7 @@ namespace TextRPG_18
                     switch (input)
                     {
                         case 1:
+                            player.job = "광전사";
                             player.SelectedClass = new Warrior("광전사", 100, 30, 10, 10, 15, 130); //hp,mp,atk,def,치확,치피
                             warrior = (Warrior)player.SelectedClass;
                             warrior.Pick(player);
@@ -38,7 +39,7 @@ namespace TextRPG_18
                             Out = true;
                             break;
                         case 2:
-                            
+                            player.job = "용기사";
                             player.SelectedClass = new Kinght("용기사", 130, 30, 15, 5, 25, 160);
                             kingth = (Kinght)player.SelectedClass;
                             kingth.Pick(player);
@@ -47,7 +48,8 @@ namespace TextRPG_18
                             break;
 
                         case 3:
-                            player.SelectedClass = new Mage("원소 마법사", 80, 60, 5, 3, 25, 160);
+                            player.job = "원소 마법사";
+                            player.SelectedClass = new Mage("원소 마법사", 80, 60, 5, 3, 35, 170);
                             mage = (Mage)player.SelectedClass;
                             mage.Pick(player);
                             Console.WriteLine($"{player.name}님의 직업은 {mage.name} 입니다");
